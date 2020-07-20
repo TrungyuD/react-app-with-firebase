f2b5af5428dbdb1d5c32b4f30233fd0e78fded44
TrungyuD <shawnkool10@gmail.com>
2020-07-20 22:35:33 +0700

    #15 Creating a Firebase Project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/auth'
+var firebaseConfig = {
+    apiKey: "AIzaSyDxJcnYnSQhliQ-ctMouzKiztAtfcdf3_U",
+    authDomain: "manage-project-personal.firebaseapp.com",
+    databaseURL: "https://manage-project-personal.firebaseio.com",
+    projectId: "manage-project-personal",
+    storageBucket: "manage-project-personal.appspot.com",
+    messagingSenderId: "56706370913",
+    appId: "1:56706370913:web:e992f283d16547fe3a8920",
+    measurementId: "G-18MVB48MM0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({timestampsInSnapshots: true})
+  firebase.analytics();
+
+export default firebase;
