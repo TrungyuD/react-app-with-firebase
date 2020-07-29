442b1454fd09a7c77a47877b1030ac9a0a380dbd
TrungyuD <shawnkool10@gmail.com>
2020-07-29 23:26:21 +0700

    #19 & #20 Sync Project Details Data

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,22 +1,47 @@
 import React from 'react'
+import {connect} from 'react-redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import {compose} from 'redux';
 
 const ProjectDetails = (props) => {
-    const {id} = props.match.params;
-    return (
-        <div className="container section project-details">
+    const {project} = props;
+    if (project){
+        return(
+            <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project Title - {id}</span>
-                    <p>lorem ipsum</p>
+                    <span className="card-title">{project.title}</span>
+                    <p>{project.content}</p>
                 </div>
                 <div className="card-action gret lighten-4 grey-text">
-                    <div>Posted by NTD</div>
+                    <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
                     <div>15th July, 2020</div>
                 </div>
             </div>
         </div>
-    )
+        )
+    } else {
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+        )
+    }
+    
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+    console.log(state);
+    const {id} = ownProps.match.params;
+    const {projects} = state.firestore.data;
+    const project = projects ? projects[id] : null;
+    return {
+        project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect(()=>['projects'])
+)(ProjectDetails)
 
--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import {reduxFirestore, getFirestore} from 'redux-firestore'
-import {reactReduxFirebase, getFirebase} from 'react-redux-firebase'
+
+import {reduxFirestore, getFirestore, createFirestoreInstance} from 'redux-firestore'
+import { getFirebase, ReactReduxFirebaseProvider} from 'react-redux-firebase'
 import fbConfig from './config/fbConfig';
 import firebase from 'firebase/app';
 const store = createStore(rootReducer,
@@ -19,12 +20,26 @@ const store = createStore(rootReducer,
   )
 );
 
+const rrfConfig = {
+  userProfile: 'users',
+}
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+
+
+}
+
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <ReactReduxFirebaseProvider {...rrfProps}>
+          <App />
+      </ReactReduxFirebaseProvider>
   </Provider>,
-  document.getElementById('root')
-);
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
