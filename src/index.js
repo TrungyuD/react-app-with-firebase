4c97f50112b185d24f016c54e25e1a31077c57e0
TrungyuD <shawnkool10@gmail.com>
2020-08-01 17:11:26 +0700

    #26 Waiting for Auth Ready

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
-import { Provider } from 'react-redux';
+import { Provider,useSelector } from 'react-redux';
 import thunk from 'redux-thunk';
-
+import { isLoaded  } from 'react-redux-firebase';
 import {reduxFirestore, getFirestore, createFirestoreInstance} from 'redux-firestore'
-import { getFirebase, ReactReduxFirebaseProvider} from 'react-redux-firebase'
+import { getFirebase, ReactReduxFirebaseProvider, reactReduxFirebase} from 'react-redux-firebase'
 import fbConfig from './config/fbConfig';
 import firebase from 'firebase/app';
 const store = createStore(rootReducer,
@@ -28,18 +28,25 @@ const rrfProps = {
   config: rrfConfig,
   dispatch: store.dispatch,
   createFirestoreInstance,
-
-
+  userProfile: 'users', // where profiles are stored in database
+  presence: 'presence', // where list of online users is stored in database
+  sessions: 'sessions'
+}
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+      return children
 }
 
+ReactDOM.render(<Provider store={store}> 
+                  <ReactReduxFirebaseProvider {...rrfProps}> 
+                    <AuthIsLoaded>
+                      <App /> 
+                    </AuthIsLoaded>
+                  </ReactReduxFirebaseProvider>
+                </Provider>, 
+              document.getElementById('root'));
 
-ReactDOM.render(
-  <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-          <App />
-      </ReactReduxFirebaseProvider>
-  </Provider>,
-  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
