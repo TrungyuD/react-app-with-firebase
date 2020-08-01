2cd48aae51be531caf24ff4d5a7059e3b3f7e043
TrungyuD <shawnkool10@gmail.com>
2020-08-01 15:13:15 +0700

    #23 Logging Users In

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,6 +1,23 @@
-const initState = {}
+const initState = {
+    authError : null
+}
 const authReducer = (state = initState, action) => {
-
+    switch(action.type){
+        case 'LOGIN_ERROR':
+            console.log('Login failed');
+            return {
+                ...state,
+                authError: 'Login faild'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('Login success');
+            return {
+                ...state,
+                authError : null
+            }
+        default: 
+            return state
+    }
     return state;
 }
 
