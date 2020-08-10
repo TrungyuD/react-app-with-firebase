fb816edc4d3b4030ef976ef51ef25a43e6862d1a
TrungyuD <shawnkool10@gmail.com>
2020-08-10 21:51:15 +0700

    finish project

---
+++ dist/precache-manifest.d83933776b7f419e3ae7225e239a44b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173c4ad11d8e81b52a1630b5f728b1ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e33beb3ee5d102f46b35",
+    "url": "/static/css/main.22dd5560.chunk.css"
+  },
+  {
+    "revision": "58f7422749d7d6533ef8",
+    "url": "/static/js/2.6568454d.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.6568454d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33beb3ee5d102f46b35",
+    "url": "/static/js/main.c1f16996.chunk.js"
+  },
+  {
+    "revision": "68f4705f4d705106cc7f",
+    "url": "/static/js/runtime-main.bc0faf6a.js"
+  }
+]);
